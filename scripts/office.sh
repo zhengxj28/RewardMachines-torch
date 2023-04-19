0cf3c8e6ab4c7707b6f87cb5f83a249c6cf5ac02
zhengxj28 <83148317+zhengxj28@users.noreply.github.com>
2023-04-19 14:07:14 +0800

    update bash shell

--- scripts/office.sh
+++ scripts/office.sh
@@ -1,5 +1,5 @@
 cd src
-for i in `seq 0 1`;
+for i in `seq 0 9`;
 do
 	nohup python -u run.py --algorithm="qrm" --world="office" --seed=$i --use_wandb >../logs/office/qrm_seed$i.log 2>&1 &&
 	nohup python -u run.py --algorithm="qrm-rs" --world="office" --seed=$i --use_wandb >../logs/office/qrmrs_seed$i.log 2>&1
--- scripts/water_qrmrs.sh
+++ scripts/water_qrmrs.sh
@@ -1,5 +1,5 @@
 cd src
-for i in `seq 0 1`;
+for i in `seq 0 9`;
 do
-	nohup python -u run.py --algorithm="qrm-rs" --world="water" --seed=$i --use_wandb >../logs/water/qrmrs_seed$i.log 2>&1 &
+	nohup python -u run.py --algorithm="qrm-rs" --world="water" --seed=$i --use_wandb >../logs/water/qrmrs_seed$i.log 2>&1 &&
 done
