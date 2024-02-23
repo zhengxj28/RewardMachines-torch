e18fc7db1071aad55516b22f06c1be61e720ed01
zhengxj28 <zhengxj28@mail2.sysu.edu.cn>
2024-02-23 17:04:17 +0800

    update scripts

--- scripts/office/stochastic/qrm.sh
+++ scripts/office/stochastic/qrm.sh
@@ -1,2 +1,2 @@
 cd src
-python -u run.py --project "Stochastic Reward Machines" --algorithm="qrm" --load_rm_mode "srm" --world="office" --label_noise 0 --use_wandb --use_cuda --seeds 0 1 2 3 >../logs/office/qrm.log 2>&1;
+nohup python -u run.py --project "Stochastic Reward Machines" --algorithm="qrm" --load_rm_mode "srm" --world="office" --label_noise 0 --use_wandb --use_cuda --seeds 0 1 2 3 >../logs/office/qrm.log 2>&1 &
